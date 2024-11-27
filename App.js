0f18bb91ef58b5ab19e3e68eea628cd015a6e342
Gabriel Carvalho <110797653+Gabriel5595@users.noreply.github.com>
2024-11-27 22:33:54 +0100

    Final commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import { Text, SafeAreaView} from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <Text >
+        TP3.1
+      </Text>
+    </SafeAreaView>
+  );
+}
